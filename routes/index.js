ca784db7b1e4cd3186aaff4d08a60a9a95a8f498
Jalaj Kumar Gaur <raajk0338@gmail.com>
2021-06-16 21:48:24 +0530

    v16062021

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+//GET Routes
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index');
 });
+
+/* GET emporium page. */
+router.get('/emp', function (req, res, next) {
+  res.render('emporium');
+});
+
+/* GET farmingTips page. */
+router.get('/farmingTips', function (req, res, next) {
+  res.render('farmingTips');
+});
+
+/* GET landDetails/login page. */
+router.get('/login', function (req, res, next) {
+  res.render('loginPage');
+});
+
+/* GET landDetails/login page. */
+router.get('/reg', function (req, res, next) {
+  res.render('registerPage');
+});
+
+
 
 
 module.exports = router;
